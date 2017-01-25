3519f3200c72c12f7ef625120c3c904c0f1e6552
marcusljx <marcusljx@gmail.com>
2017-01-26 07:47:15 +0800

    fixed protoparser test

--- protoparser/ProtoParser_test.go
+++ protoparser/ProtoParser_test.go
@@ -1,14 +1,17 @@
 package protoparser
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
-const (
-	testProtoFile = "_examples/sample.proto"
+var (
+	gopath        = os.Getenv("GOPATH")
+	testProtoFile = filepath.Join(gopath, "src/github.com/marcusljx/grpcgen-go/_examples/sample.proto")
 )
-
 
 func TestReadRPCs(t *testing.T) {
 	if testing.Short() {
@@ -16,9 +19,9 @@ func TestReadRPCs(t *testing.T) {
 	}
 
 	rpcs := ReadRPCs(testProtoFile)
-	assert.Equal(t, "ProcedureCall_1",rpcs[0].Name)
+	assert.Equal(t, "ProcedureCall_1", rpcs[0].Name)
 	assert.False(t, rpcs[0].Input.IsStream)
 
-	assert.Equal(t, "ProcedureCall_2",rpcs[1].Name)
+	assert.Equal(t, "ProcedureCall_2", rpcs[1].Name)
 	assert.False(t, rpcs[1].Input.IsStream)
 }
